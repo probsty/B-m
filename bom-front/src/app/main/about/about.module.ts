59ed5cb3b227450ccd76b7858c8e778cf1c27bdf
Guillaume Monot <guillaumemonotxy@gmail.com>
2020-06-08 22:57:51 +0200

    feat: add contact component

--- bom-front/src/app/main/about/about.module.ts
+++ bom-front/src/app/main/about/about.module.ts
@@ -2,16 +2,21 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { QuiSommesNousComponent } from './qui-sommes-nous/qui-sommes-nous.component';
 import { RouterModule } from '@angular/router';
+import { ContactComponent } from './contact/contact.component';
 
 const routes = [
     {
       path: 'qui-sommes-nous',
       component: QuiSommesNousComponent
+    },
+    {
+        path: 'contact',
+        component: ContactComponent
     }
   ];
 
 @NgModule({
-  declarations: [QuiSommesNousComponent],
+  declarations: [QuiSommesNousComponent, ContactComponent],
   imports: [
     CommonModule,
     RouterModule.forChild(routes)
