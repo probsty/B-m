3d50630f5951d8b5f693d75e999b22e9b1bc098e
Guillaume Monot <guillaumemonotxy@gmail.com>
2020-06-22 00:55:00 +0200

    feat: add filtering function on ngx datatable

--- bom-front/src/app/main/back-office/back-office.component.ts
+++ bom-front/src/app/main/back-office/back-office.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { deburr } from 'lodash';
 
+import { MatDialogRef, MatDialog } from '@angular/material/dialog';
+import { ConfirmDeleteDialogComponent } from 'src/app/global/comfirm-delete-dialog/confirm-delete-dialog.component';
+
 @Component({
   selector: 'app-back-office',
   templateUrl: './back-office.component.html',
@@ -14,7 +17,9 @@ export class BackOfficeComponent implements OnInit {
 
   temp: any;
 
-  constructor(private _router: Router) {}
+  dialogDeleteRef: MatDialogRef<ConfirmDeleteDialogComponent>;
+
+  constructor(private _dialog: MatDialog, private _router: Router) {}
 
   private refreshDataTable(): void {
     console.log('didClick or has been affected by refresh table');
@@ -22,12 +27,18 @@ export class BackOfficeComponent implements OnInit {
     this.rowsData = [
       {
         nom: 'MONOT',
+        prenom: 'Guillaume',
+        email: 'guillaume.monot@epitech.eu',
       },
       {
         nom: 'PROBST',
+        prenom: 'Yann',
+        email: 'yann.probst@epitech.eu',
       },
       {
         nom: 'DELANE',
+        prenom: 'Julien',
+        email: 'julien.delane@epitech.eu',
       },
     ];
     this.temp = this.rowsData;
@@ -39,7 +50,7 @@ export class BackOfficeComponent implements OnInit {
 
   detailUser(event: any): void {
     // this._router.navigate(['/']);
-    console.log('did click on', event.selected[0].nom)
+    console.log('did click on', event.selected[0].nom);
   }
 
   updateFilter(event: any, prop: string): void {
@@ -59,5 +70,26 @@ export class BackOfficeComponent implements OnInit {
         }).length === Object.keys(this.filters).length
       );
     });
+  }
+
+  onEditUser(row: any): void {
+    console.log(row);
+  }
+
+  dialogDelete(row: any): void {
+    this.dialogDeleteRef = this._dialog.open(ConfirmDeleteDialogComponent, {
+      disableClose: true,
+    });
+
+    this.dialogDeleteRef.afterClosed().subscribe((data) => {
+      if (data) {
+        this.deleteUser(row);
+      }
+    });
+  }
+
+  deleteUser(row: any): void {
+    this.refreshDataTable();
+    console.log('Row deleted');
   }
 }
