8bf38175fba699956a06dafc6e9144079ab3b417
Guillaume Monot <guillaumemonotxy@gmail.com>
2020-06-22 00:30:47 +0200

    feat: add premisces of BO on users admin

--- bom-front/src/app/main/back-office/back-office.component.ts
+++ bom-front/src/app/main/back-office/back-office.component.ts
@@ -1,15 +1,63 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { deburr } from 'lodash';
 
 @Component({
   selector: 'app-back-office',
   templateUrl: './back-office.component.html',
-  styleUrls: ['./back-office.component.sass']
+  styleUrls: ['./back-office.component.sass'],
 })
 export class BackOfficeComponent implements OnInit {
+  rowsData: any;
+  filters = {};
+  filteredRows: any;
 
-  constructor() { }
+  temp: any;
 
-  ngOnInit(): void {
+  constructor(private _router: Router) {}
+
+  private refreshDataTable(): void {
+    console.log('didClick or has been affected by refresh table');
+
+    this.rowsData = [
+      {
+        nom: 'MONOT',
+      },
+      {
+        nom: 'PROBST',
+      },
+      {
+        nom: 'DELANE',
+      },
+    ];
+    this.temp = this.rowsData;
   }
 
+  ngOnInit(): void {
+    this.refreshDataTable();
+  }
+
+  detailUser(event: any): void {
+    // this._router.navigate(['/']);
+    console.log('did click on', event.selected[0].nom)
+  }
+
+  updateFilter(event: any, prop: string): void {
+    const val = deburr(event.target.value.toLowerCase());
+    this.filters[prop] = val;
+    this.applyFilters();
+  }
+
+  applyFilters(): void {
+    this.rowsData = this.temp.filter((d: any) => {
+      return (
+        Object.keys(this.filters).filter((p) => {
+          return (
+            deburr(d[p].toLowerCase()).indexOf(this.filters[p]) !== -1 ||
+            !this.filters[p]
+          );
+        }).length === Object.keys(this.filters).length
+      );
+    });
+  }
 }
--- bom-front/src/app/main/back-office/back-office.module.ts
+++ bom-front/src/app/main/back-office/back-office.module.ts
@@ -2,19 +2,37 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BackOfficeComponent } from './back-office.component';
 import { RouterModule } from '@angular/router';
+import { httpInterceptorProviders } from 'src/app/http-interceptors';
+import { NgxDatatableModule } from '@swimlane/ngx-datatable';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSelectModule } from '@angular/material/select';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 const routes = [
-    {
-      path: 'back-office',
-      component: BackOfficeComponent,
-    },
-  ];
+  {
+    path: 'back-office',
+    component: BackOfficeComponent,
+  },
+];
 
 @NgModule({
   declarations: [BackOfficeComponent],
   imports: [
     CommonModule,
-    RouterModule.forChild(routes)
-  ]
+    RouterModule.forChild(routes),
+
+    NgxDatatableModule,
+    MatButtonModule,
+    MatIconModule,
+    MatSelectModule,
+    MatExpansionModule,
+    MatFormFieldModule,
+    MatInputModule
+
+  ],
+  providers: [httpInterceptorProviders],
 })
-export class BackOfficeModule { }
+export class BackOfficeModule {}
