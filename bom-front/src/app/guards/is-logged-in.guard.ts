7f1a9c8fa08cb1caefdb8056bb02f2e37fd6aab1
Guillaume Monot <guillaumemonotxy@gmail.com>
2020-06-25 18:05:38 +0200

    feat: add feature on is logged in guard

--- bom-front/src/app/guards/is-logged-in.guard.ts
+++ bom-front/src/app/guards/is-logged-in.guard.ts
@@ -1,29 +1,45 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
-import { Observable } from 'rxjs';
+import {
+  CanActivate,
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot,
+  UrlTree,
+  Router,
+} from '@angular/router';
+import { Observable, Subject } from 'rxjs';
 // import { AuthService } from 'app/services/auth/auth.service';
 
 import { map } from 'rxjs/operators';
+import { UsersService } from '../services/users/users.service';
 // import { SnackBarService } from 'app/services/snack-bar/snack-bar.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class IsLoggedInGuard implements CanActivate {
-
   constructor(
     private _router: Router,
-    // private _authService: AuthService,
-    // private _snackBar: SnackBarService,
+    private _userService: UsersService // private _authService: AuthService, // private _snackBar: SnackBarService,
   ) {}
 
   canActivate(
     next: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot,
-  ): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-      console.log(`Don't care`);
-    //   this._router.navigate(['/']);
-      return true;
+    state: RouterStateSnapshot
+  ):
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree>
+    | boolean
+    | UrlTree {
+    this._userService.getCurrentUser().subscribe(
+      (user) => {
+        if (!user.id) {
+          this._router.navigate(['/']);
+        }
+      },
+      (error) => {
+        this._router.navigate(['/']);
+      }
+    );
+    return true;
   }
-
 }
