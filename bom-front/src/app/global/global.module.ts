8e06512a2e72eca3779ce06420a88d177ebbc96b
Guillaume Monot <guillaumemonotxy@gmail.com>
2020-06-28 01:19:15 +0200

    feat: add temoignage reusable component

--- bom-front/src/app/global/global.module.ts
+++ bom-front/src/app/global/global.module.ts
@@ -4,10 +4,11 @@ import { CarouselComponent } from './carousel/carousel.component';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { ProductCardComponent } from './product-card/product-card.component';
 import { MatCardModule } from '@angular/material/card';
+import { TemoignageComponent } from './temoignage/temoignage.component';
 
 @NgModule({
   imports: [CommonModule, FlexLayoutModule, MatCardModule],
-  declarations: [CarouselComponent, ProductCardComponent],
+  declarations: [CarouselComponent, ProductCardComponent, TemoignageComponent],
   exports: [CarouselComponent, ProductCardComponent],
 })
 export class GlobalModule {}
--- bom-front/src/app/global/temoignage/temoignage.component.ts
+++ bom-front/src/app/global/temoignage/temoignage.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-import { TemoignageService } from 'src/app/services/temoignage/temoignage.service';
 
 @Component({
   selector: 'app-temoignage',
   templateUrl: './temoignage.component.html',
-  styleUrls: ['./temoignage.component.sass'],
+  styleUrls: ['./temoignage.component.sass']
 })
 export class TemoignageComponent implements OnInit {
-  constructor(private _temoignageService: TemoignageService) {}
+
+  constructor() { }
 
   ngOnInit(): void {
-    
   }
+
 }
