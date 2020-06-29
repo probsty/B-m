71630efb6e589e17c81bfd00e726331c6ab4d053
Yann Probst <yann.probst@epitech.eu>
2020-06-29 05:56:35 -0800

    feat: temoignages sections

--- bom-front/src/app/global/temoignage/temoignage.component.ts
+++ bom-front/src/app/global/temoignage/temoignage.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { TemoignageService } from 'src/app/services/temoignage/temoignage.service';
+import {AuthService} from "../../services/auth/auth.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'temoignage',
@@ -6,10 +9,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./temoignage.component.scss']
 })
 export class TemoignageComponent implements OnInit {
+  rowsTemoignageData: any;
 
-  constructor() { }
+  constructor(
+    private _authService: AuthService,
+    private _router: Router,
+    private _temoignageService: TemoignageService
+  ) { }
 
-  ngOnInit(): void {
+  /*
+   * Get the temoignages from the api and save the content
+   */
+  private refreshDataTable(): void {
+    console.log("Je fais refreshDataTable")
+    this._temoignageService.getAll().subscribe(
+      (posts: any) => {
+        console.log(posts);
+        this.rowsTemoignageData = posts.filter((post: any) => {
+          return post.tags.includes('temoignage');
+        });
+      },
+      (err) => {
+        console.log('An error occured while fetching Temoignage', err);
+      }
+    );
   }
 
+  ngOnInit(): void {
+    this.refreshDataTable();
+    console.log(this.rowsTemoignageData);
+  }
 }
