b8e451b8a1a2ffcd876c2739d06846f2e34cc04e
Guillaume Monot <guillaumemonotxy@gmail.com>
2020-07-02 22:57:44 +0200

    feat: fetch clicked product on init

--- bom-front/src/app/main/produit/produit.component.ts
+++ bom-front/src/app/main/produit/produit.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ProduitService } from 'src/app/services/produit/produit.service';
 
 @Component({
   selector: 'app-produit',
   templateUrl: './produit.component.html',
-  styleUrls: ['./produit.component.sass']
+  styleUrls: ['./produit.component.sass'],
 })
 export class ProduitComponent implements OnInit {
+  id: any;
+  product: any;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private _produitService: ProduitService
+  ) {}
 
   ngOnInit(): void {
+    this.route.params.subscribe((res) => {
+      this.id = res.id;
+      this._produitService.getProduct(this.id).subscribe(
+        (product) => {
+          this.product = product;
+        },
+        (err) => {
+          console.log('An error occured while fetching product', err);
+        }
+      );
+    });
   }
-
 }
