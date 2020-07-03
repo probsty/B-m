20215914394b04354ef966d163fafff47daa3db3
Guillaume Monot <guillaumemonotxy@gmail.com>
2020-07-03 19:23:16 +0200

    feat: add edit user

--- bom-front/src/app/main/account-manager/profile/profile.component.ts
+++ bom-front/src/app/main/account-manager/profile/profile.component.ts
@@ -1,15 +1,117 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { MyErrorStateMatcher } from '../register/state-matcher.component';
+import { MatDialogRef, MatDialog } from '@angular/material/dialog';
+import { DialogComponent } from 'src/app/global/dialog/dialog.component';
+import { AuthService } from 'src/app/services/auth/auth.service';
+import { Router } from '@angular/router';
+import { UsersService } from 'src/app/services/users/users.service';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.sass']
+  styleUrls: ['./profile.component.sass'],
 })
 export class ProfileComponent implements OnInit {
+  userForm: FormGroup;
+  matcher = new MyErrorStateMatcher();
+  dialog: MatDialogRef<DialogComponent>;
+  user: any;
 
-  constructor() { }
+  constructor(
+    private _userService: UsersService,
+    private _formBuilder: FormBuilder
+  ) {}
 
-  ngOnInit(): void {
+  /*
+   * Initiation of the component
+   */
+  ngOnInit() {
+    this.userForm = this._formBuilder.group(
+      {
+        email: ['', [Validators.required]],
+        username: ['', [Validators.required]],
+        password: [''],
+        confirmPassword: [''],
+      },
+      { validator: this.checkPasswords }
+    );
+    this.refreshData();
   }
 
+  private refreshData(): void {
+    this._userService.getCurrentUser().subscribe(
+      (user) => {
+        this.user = user;
+
+        this.userForm.patchValue({
+          email: user.email,
+          username: user.username,
+        });
+      },
+      (err) => {
+        console.log(
+          'An error occured while fetching current user information',
+          err
+        );
+      }
+    );
+  }
+
+  /*
+   * Submit the forms
+   */
+  onRegister(): void {
+    const data = {
+      ...this.user,
+      email: this.userForm.get('email').value,
+      username: this.userForm.get('username').value,
+    };
+    if (this.userForm.get('password').value) {
+      data['password'] = this.userForm.get('password').value;
+    }
+    this._userService.editCurrentUser(data).subscribe(
+      () => {
+        this.refreshData();
+      },
+      (err) => {
+        console.log('An error occured while editing user information', err);
+      }
+    );
+  }
+
+  /*
+   * Custom validator for checking the passwords
+   */
+  checkPasswords(group: FormGroup) {
+    return null;
+  }
+
+  /*
+   * Get username content in my register.component.html file
+   */
+  get username() {
+    return this.userForm.get('username');
+  }
+
+  /*
+   * Get email content in my register.component.html file
+   */
+  get email() {
+    return this.userForm.get('email');
+  }
+
+  /*
+   * Get password content in my register.component.html file
+   */
+  get password() {
+    return this.userForm.get('password');
+  }
+
+  /*
+   * Get confirmPassword content in my register.component.html file
+   */
+  //   get confirmPassword() {
+  //     return this.userForm.get('confirmPassword');
+  //   }
 }
