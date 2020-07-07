28a1a37f0ece064d50ad9317f6d099333cb77a38
Guillaume Monot <guillaumemonotxy@gmail.com>
2020-07-07 12:32:31 +0200

    feat: add toolbar-item reusable component

---
+++ bom-front/src/app/global/toolbar-item/toolbar-item.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-toolbar-item',
+  templateUrl: './toolbar-item.component.html',
+  styleUrls: ['./toolbar-item.component.sass']
+})
+export class ToolbarItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
